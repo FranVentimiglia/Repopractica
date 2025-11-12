585ccc325b49a58158892811f86fc6f1d4b8d187
Fran <fventimiglia@w3itsolutions.net>
2025-11-12 01:29:31 -0300

    Ejercicio 3 terminado

---
+++ ejerciciosJs/ejercicio3.js
@@ -0,0 +1,22 @@
+/*Definir una función que cree una cadena de letras repetidas tantas veces
+como le digamos. 
+ 
+La función recibe dos argumentos: la letra y el número de repeticiones.*/
+
+function repetirLetras(letra, repeticiones) {
+    let cadena;
+
+    for (let i = 0; i < repeticiones; i++) {
+        cadena = + letra;
+    } return cadena
+
+}
+
+let letra;
+let repeticiones;
+
+letra = prompt("Ingresa una letra");
+repeticiones = prompt("Ingresa la cantidad de repeticiones");
+
+Console.log("Su cadena es:" + repetirLetras(letra,repeticiones));
+
